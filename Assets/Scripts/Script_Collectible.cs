8e5d596c20938bbc495cacebef22758cbd1d70c0
Andrew Lai <the.andrew.lai@gmail.com>
2024-09-12 21:32:28 -0700

    Added MeatLoops cereal box

--- Assets/Scripts/Script_Collectible.cs
+++ Assets/Scripts/Script_Collectible.cs
@@ -15,7 +15,7 @@ public class Script_Collectible : MonoBehaviour
     }
     void Update()
     {
-        transform.localRotation = Quaternion.Euler(90f, Time.time * 100f, 0);
+        transform.localRotation = Quaternion.Euler(-90f, Time.time * 100f, 0);
     }
 
     void OnTriggerEnter(Collider other)
